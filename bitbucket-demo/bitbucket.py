b6a8ee5b1f4fec7a23cae63dc4aef0814fc3510a
Carla Buongiorno <carla.buongiorno@fox-it.com>
2023-10-17 11:14:34 +0200

    api wip

--- bitbucket-demo/bitbucket.py
+++ bitbucket-demo/bitbucket.py
@@ -1,28 +1,46 @@
 import requests
-from pprint import pprint
+from typing import Any
 
-def main():
+
+def main() -> None:
     url = 'https://bitbucket.detact.fox.local/rest'
-    path = '/api/latest/projects/CUSTOMER/repos/sapphire/commits'
-
+    path = '/api/latest/projects/CUSTOMER/repos'
+    headers = {'Authorization': 'Bearer ThisIsNotMyRealBearerToken'}
     args = {'start': 0, 'limit': 50}
-    headers = {'Authorization': 'Bearer Mzg2NTMxNzI4NDEwOqfEtFqcuiLq5eiGYVwz77vzJj1J'}
-
-    r = requests.get(url+path, params=args, headers=headers)
-
-    authors = get_author(r.json())
-    print(authors)
+    r = requests.get(url+path, headers=headers)
+    customers = get_customer(r.json())
+    customer_paths = get_customer_path(customers)
+    obj_list = get_obj(customer_paths, url, path, args, headers)
+    authors = get_author(obj_list)
     name_commits = get_author_dict(authors)
-    
+    # while not obj_list['isLastPage']:
+    #     args['start'] = obj_list['nextPageStart']
     for name in name_commits:
         print(f'{name} made {name_commits[name]} commits')
-    
 
-def get_author(obj):
-    authors = [author['author']['name'] for author in obj['values']]
+
+def get_customer_path(customers: list[Any]) -> list[str]:
+    customer_paths = [f'/{customer}/commits' for customer in customers]
+    return customer_paths
+
+
+def get_customer(obj: Any) -> list[str]:
+    customers = [customer['slug'] for customer in obj['values']]
+    return customers
+
+
+def get_obj(customer_paths: list[str], url: str, path: str, args: dict[str, int], headers: dict[str, str]) -> list[Any]:
+    obj_list = [requests.get(url+path+customer_path, params=args, headers=headers).json() for customer_path in customer_paths]
+    obj = [obj for obj in obj_list]
+    return obj
+
+
+def get_author(obj_list: list[dict[str, list[Any]]]) -> list[str]:
+    authors = [author['author']['name'] for obj in obj_list for author in obj['values']]
     return authors
 
-def get_author_dict(authors):
+
+def get_author_dict(authors: list[str]) -> dict[str, int]:
     name_commits = {}
     for name in authors:
         name_commits[name] = authors.count(name)
--- bitbucket-demo/test_bitbucket.py
+++ bitbucket-demo/test_bitbucket.py
@@ -1,16 +1,39 @@
-from bitbucket import get_author, get_author_dict
+from bitbucket import (get_author, get_author_dict,
+                       get_customer, get_customer_path)
+from typing import Any
 
-def test_get_author():
-    obj = {'values': [
-        {'author': {'name': 'iwan.intgroen'}},
-        {'author': {'name': 'pavlos.platanias'}}
-        ]}
-    expected_list = ['iwan.intgroen', 'pavlos.platanias']
-    authors = get_author(obj)
+def test_get_author() -> None:
+    obj_list = [{'values': [{'author': {'name': 'iwan.intgroen', 'id': 133}, 'displayId': '87e9bd0bb60'},
+                            {'author': {'name': 'pavlos.platanias', 'type': 'NORMAL'}, 'authorTimestamp': 1685616855000}]},
+                 {'values': [{'author': {'name': 'joris.vandenoever', 'id': 143}, 'displayId': '87e9bd0ba60'},
+                            {'author': {'name': 'pavlos.platanias', 'type': 'NORMAL'},'authorTimestamp': 1685616855009}]}]
+    expected_list = ['iwan.intgroen', 'pavlos.platanias', 'joris.vandenoever', 'pavlos.platanias']
+    authors = get_author(obj_list)
     assert authors == expected_list
 
-def test_get_author_dict():
+def test_get_author_dict() -> None:
     authors = ['iwan.intgroen', 'pavlos.platanias', 'pavlos.platanias']
     expected_dict = {'iwan.intgroen': 1, 'pavlos.platanias': 2}
     name_commits = get_author_dict(authors)
     assert name_commits == expected_dict
+
+def test_get_customer() -> None:
+    obj = {'values': [ {'forkable': True, 'slug': 'blue'},
+                             {'id': 29, 'slug': 'denim'}]}
+    expected_customers = ['blue', 'denim']
+    customers = get_customer(obj)
+    assert customers == expected_customers
+
+def test_get_customer_path() -> None:
+    customers = ['blue', 'denim']
+    customer_paths = get_customer_path(customers)
+    assert customer_paths == ['/blue/commits', '/denim/commits']
+
+# def test_get_obj():
+#     expected_url = 'http://bitbucket.com'
+#     path = '/api/CUSTOMER/repos'
+#     args = {'start': 0, 'limit': 2}
+#     headers = {'Authorization': 'Bearer MFqcuiLq5eiGYVwz77vzJj1J'}
+#     customer_paths = ['/blue/commits', '/denim/commits']
+#     obj = get_obj(customer_paths, expected_url, path, args, headers)
+
